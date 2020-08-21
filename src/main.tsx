f6e9caff6eeb66bf6e67fb0777e13b5b8261a4b0
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-08-22 00:19:26 +0900

    call form component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import {render} from 'react-dom'
 import * as React from 'react'
+import { Form } from './component/stateful/Form/Form'
+
+if (process.env.NODE_ENV === 'development') {
+  const {worker} = require('./mock/browser')
+  worker.start()
+}
 
 const rootElement = document.querySelector('#main')
-
-render(<div>test</div>, rootElement)
+render(<div><Form /></div>, rootElement)
